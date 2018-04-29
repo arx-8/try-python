9aa87ddc68603e779ceed0dfa94d90ef539787b0
swan0 <swans.randomness@gmail.com>
2018-04-29 17:46:00 +0900

    add build script

---
+++ file_read/build.sh
@@ -0,0 +1,8 @@
+# build
+pyinstaller file_validate.py  --onefile
+
+## Usage
+#### install
+# pip install pyinstaller
+#### execute
+# file_validate.exe X:\\dev\\try-line-api\\file_read\\file.txt 10
